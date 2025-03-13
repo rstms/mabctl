f12142ef3282d092361506824635e01301e0f7ba
Matt Krueger <mkrueger@rstms.net>
2025-03-13 16:19:53 -0600

    Bump version: 1.5.5 → 1.5.6

--- api/controller.go
+++ api/controller.go
@@ -15,7 +15,7 @@ import (
 	"strings"
 )
 
-const Version = "1.5.5"
+const Version = "1.5.6"
 
 func mkpasswd(size int) (string, error) {
 	bytes := make([]byte, size)
--- cmd/root.go
+++ cmd/root.go
@@ -32,7 +32,7 @@ import (
 	"strings"
 )
 
-const Version = "1.5.5"
+const Version = "1.5.6"
 
 const ProgramName = "mabctl"
 
